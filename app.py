2ca80ec9cda4ce7ba37230226c540f5af51c3d41
Beth Carretta <bethcarretta@gmail.com>
2023-06-03 11:09:55 -0400

    trying out instructions in chat completions

--- app.py
+++ app.py
@@ -1,30 +1,24 @@
 import os
 import openai
 
-
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-    model='text-davinci-003',
-    prompt='What is the Spanish word for purple?'
+# Create instructions for a chat completion
+instructions_dict = {
+    "role": "system",
+    "content": "Pretend you are a Mexican Spanish language tutor. Answer only in colloquial Mexican Spanish at a CEFR(Common European Framework of Reference)-level B1 level. Ask the user what he or she would like to talk about today?"
+    }
+
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[instructions_dict],
+    frequency_penalty=0.5,
+    presence_penalty=0.5
 )
-print(response.choices[0].text)
-# {
-#   "choices": [
-#     {
-#       "finish_reason": "stop",
-#       "index": 0,
-#       "logprobs": null,
-#       "text": "\n\nMorado."
-#     }
-#   ],
-#   "created": 1685799251,
-#   "id": "cmpl-7NLgRplsD1dXQJ7AVFj7hAXFogVDh",
-#   "model": "text-davinci-003",
-#   "object": "text_completion",
-#   "usage": {
-#     "completion_tokens": 5,
-#     "prompt_tokens": 8,
-#     "total_tokens": 13
-#   }
-# }
+
+chatbot_response = completion['choices'][0]['message']['content']
+
+print(chatbot_response) # ¡Hola! ¿Qué onda? ¿De qué quieres hablar hoy en nuestra clase de español mexicano?
+
+#¡Hola! ¿Cómo estás? ¿Qué tema te gustaría hablar hoy en tu clase de español? Estoy aquí para ayudarte a mejorar tus habilidades lingüísticas y hacer que te sientas más cómodo hablando en español. ¡Cuéntame qué quieres aprender hoy!
+
